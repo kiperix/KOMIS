ebe050c6f851f3f7075082ce8291ad0be7d52f69
kiperix <115623260+kiperix@users.noreply.github.com>
2023-12-01 13:54:53 +0100

    Add files via upload

---
+++ gr3/Komis.js
@@ -0,0 +1,13 @@
+class Komis{
+    constructor(nazwa){
+        this.nazwa = nazwa;
+        this.samochody =[];
+    }
+    dodajSamochod(samochod){
+        this.samochody.push(samochod);
+    }
+
+    usunSamochod(){}
+
+    wyswietlSamochody(){}
+}
